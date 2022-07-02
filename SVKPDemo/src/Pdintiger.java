d4b73ba1306871881d1db444198f9983ab17b8d0
Admin <Admin@192.168.0.108>
2022-07-02 13:31:33 -0700

    integer

---
+++ SVKPDemo/src/Pdintiger.java
@@ -0,0 +1,25 @@
+
+public class Pdintiger {
+
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+			 
+
+					int max = 2147483647;
+					int min = -2147483648;
+					short shortMax = 32767;
+					short shortMin = -32768;
+					long longMax = 9223372036854775807L;
+					long longMin = -9223372036854775808L;
+					byte byteMax = 127;
+					byte byteMin = -128;
+					System.out.println("integer max value is : "+max);
+					System.out.println("integer min value is : "+min);
+					System.out.println("integer shortMax value is : "+shortMax);
+					System.out.println("integer shortMin value is : "+shortMin);
+					System.out.println("integer longMax value is : "+longMax);
+					System.out.println("integer longMin value is : "+longMin);
+					System.out.println("integer byteMax value is : "+byteMax);
+					System.out.println("integer ByteMin value is : "+byteMin);
+				}
+			}
