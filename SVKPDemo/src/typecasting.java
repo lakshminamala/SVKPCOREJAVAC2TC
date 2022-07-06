0815f62075e82fcfbcde878465991e5f32b1d1bd
Admin <Admin@192.168.0.101>
2022-07-06 11:26:57 -0700

    float

---
+++ SVKPDemo/src/typecasting.java
@@ -0,0 +1,10 @@
+
+public class typecasting {
+
+	public static void main(String[] args) {
+				
+		int i= 10;  // variable with f is a type of integer which is holding value of 10
+		       long l = (long) i;// defining a var with name l which of datatype long = convert value that is present in I to long
+		System.out.println(l);
+			}
+		}
