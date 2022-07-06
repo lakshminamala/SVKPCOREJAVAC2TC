a05e1d4014ce36e51e446c0577d9b7fceef95445
Admin <Admin@192.168.0.101>
2022-07-06 12:06:50 -0700

    operator

---
+++ SVKPDemo/src/ternary.java
@@ -0,0 +1,27 @@
+
+public class ternary {
+
+	public static void main(String[] args) 
+			{
+				int x = 11;
+				
+				x = (10 == x) ?1 : 0;   //  condition ?: True:False
+				
+				System.out.println(x);
+
+			}
+
+		
+
+			{
+				int x = 4;
+				//x += 5; // x = x + 5
+				//x -= 5; // x = x - 5
+				//x *= 5; // x = x * 5
+				//x /= 4; // x = x / 4
+				x %= 5;   // x = x % 5
+				System.out.println(x);
+			}
+}
+
+
