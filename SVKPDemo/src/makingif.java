ba54e80039fe0792845fd77aa1eb9786fabf73ec
Admin <Admin@192.168.0.101>
2022-07-06 12:14:12 -0700

    if statement

---
+++ SVKPDemo/src/makingif.java
@@ -0,0 +1,35 @@
+
+public class makingif {
+
+	public static void main(String[] args) 
+	{
+				int x = 5;
+
+				
+				if (x == 5) 
+				  { 
+				if (x>= 5) 
+				  {
+					System.out.println("X is greater than 5"); 
+				   }
+				else
+				  {
+				System.out.println("X is smaller than 5"); 
+				  } 
+				 }
+				
+				if(x == 5)
+				{
+					System.out.println("X equals to 5");
+				}
+				if(x> 5)
+				{
+					System.out.println("X is greater to 5");		
+				}
+				else
+				{
+					System.out.println("X is smaller than 5");
+				}
+				
+			}
+}
