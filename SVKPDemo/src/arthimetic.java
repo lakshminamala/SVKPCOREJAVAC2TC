24c0f2699e81296de3df8a32abe0f218971302fd
Admin <Admin@192.168.0.101>
2022-07-06 11:55:25 -0700

    operators

---
+++ SVKPDemo/src/arthimetic.java
@@ -0,0 +1,13 @@
+
+public class arthimetic {
+
+	public static void main(String[] args) {
+				
+				int x = 10, y=20;
+				System.out.println(x+y); //30
+				System.out.println(--x); // 9
+				System.out.println(++y); //21
+
+			}
+
+		}
