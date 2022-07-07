0a00eedcc874d0c6c52eb678162cd590e34374a1
Admin <Admin@DESKTOP-FM56E02>
2022-07-06 20:19:38 -0700

    while

---
+++ SVKPDemo/src/whileloop.java
@@ -0,0 +1,31 @@
+
+public class whileloop {
+
+	public static void main(String[] args) {
+				int i=99;
+				while(i<=100)
+				{
+					System.out.println(i);
+					i++;
+				}
+	}
+
+
+		// While Loop 
+			{
+				int i = 11;
+				do
+				{
+					System.out.println(i);
+					i++;
+				}while(i<=1);
+			}
+		
+
+
+
+
+
+	}
+
+
