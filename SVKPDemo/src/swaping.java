b115c8091d26be3b1d53e8f7cddcfe2446be444d
Admin <Admin@DESKTOP-FM56E02>
2022-07-11 07:52:31 -0700

    swap two variables

---
+++ SVKPDemo/src/swaping.java
@@ -0,0 +1,20 @@
+
+public class swaping {
+
+	public static void main(String[] args) {
+		 
+		        int x = 12, y = 43;
+		 
+		        System.out.println("Before Swap");
+		        System.out.println("x = " + x);
+		        System.out.println("y = " + y);
+		 
+		        int temp = x;
+		        x = y;
+		        y = temp;
+		 
+		        System.out.println("After swap");
+		        System.out.println("x = " + x);
+		        System.out.println("y = " + y);
+		    }
+}
