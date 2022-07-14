d7cbd55ac7a4d1520ffa55d88a7d4a793734e374
Admin <Admin@192.168.0.108>
2022-07-14 12:17:37 -0700

    positive or negative

---
+++ SVKPDemo/src/positiveornegative.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class positiveornegative {
+
+	public static void main(String[] args) {
+				int i;
+				Scanner sc= new Scanner(System.in);
+				System.out.print("enter the number:-  ");
+				 i = sc.nextInt();
+				 if(i>0)
+				 {
+					 System.out.println("positive number");
+				 }
+				 else if(i<0)
+				 {
+					 System.out.println("negitive number");
+				 }
+				 else
+				 {
+					 System.out.println("zero");
+				 }
+
+	}
+}
